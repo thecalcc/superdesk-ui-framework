1127a4a2407234024bc826ca73b9b1f68daaf222
Konstantin Markov <maga.ot.oz@protonmail.com>
2024-10-29 15:21:33 +0200

    Fix lint

--- examples/pages/playgrounds/react-playgrounds/Multiedit.tsx
+++ examples/pages/playgrounds/react-playgrounds/Multiedit.tsx
@@ -134,7 +134,7 @@ export class Editor extends React.Component<{}, IEditor> {
                 )}
                 main={(
                     <Layout.AuthoringMain
-                        headerPadding='medium'
+                        headerStyles='authoring-header--padding-medium'
                         toolBar={(
                             <React.Fragment>
                                 <div className="sd-editor-toolbar__content">
